7892c96ab9709c52f1c30fd761ab3bfd26b6adc2
ayman-93 <aymanfaisal.cs@gmail.com>
2019-02-11 10:26:27 +0300

    added auther

--- quotes.js
+++ quotes.js
@@ -4,4 +4,7 @@ axios({
     "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en"
 }).then(response => {
   document.querySelector(".quote").textContent = response.data.quoteText;
+  let span = document.createElement("span");
+  span.innerHTML = "<span> By " + response.data.quoteAuthor + "</span>";
+  document.querySelector(".quote").appendChild(span);
 });
